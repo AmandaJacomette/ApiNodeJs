89e90f3faa8389c0051d2a177236be507375f88e
amanda jacomette <amandajacomett@gmail.com>
2023-09-13 19:57:12 -0300

    Commit referente aos diferentes tipos de params que podem ser passados (Aula 6)

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const server = express();
 
-server.get('/curso', (req, res) => {
+//Query params = ?nome=Node
+//Route params = /curso/2
+//Request body = {nome: 'node', tipo: 'back'}
+
+//a rota usada localhost:3000/cursos
+server.get('/curso/:id', (req, res) => {
+    //const nome = req.query.nome; // localhost:3000/curso?nome=Node.js
+    const nome = req.params.id; // localhost:3000/curso/2
     return res.json(
-        {curso: 'Node JS'}
+        {curso: `curso ${nome}`}
     );
 });
 
-server.listen(3000);
+server.listen(3000);//a porta que ele está
